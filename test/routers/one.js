6e996f009ba46a731e1316bdc7d3f032ac147ed7
Cavin <cav@cav.ru>
2018-01-07 12:03:59 +0300

    Update package.json, v0.0.4

--- test/routers/one.js
+++ test/routers/one.js
@@ -1,10 +1,10 @@
-module.exports = function (router) {
+module.exports = router => {
 
-  router.get('/sync/res.send', function (req, res) {
+  router.get('/sync/res.send', (req, res) => {
     res.send({result: 'OK'});
   });
 
-  router.get('/sync/res.send/error', function (req, res, next) {
+  router.get('/sync/res.send/error', (req, res, next) => {
     res.status(500);
     next('ERROR');
   });
@@ -13,24 +13,24 @@ module.exports = function (router) {
     return {result: 'OK'};
   });
 
-  router.get('/async/res.send', async function (req, res) {
+  router.get('/async/res.send', async (req, res) => {
     res.send({result: 'OK'});
     return;
   });
 
 
-  router.get('/async/return/error', async function (req, res) {
+  router.get('/async/return/error', async (req, res) => {
     res.status(500);
     throw 'ERROR';
   });
 
-  router.get('/async/promise', async function () {
+  router.get('/async/promise', async () => {
     return new Promise( function (resolve) {
       resolve({result: 'OK'});
     });
   });
 
-  router.get('/async/promise/error', async function () {
+  router.get('/async/promise/error', async () => {
     return new Promise( function (resolve, reject) {
       reject('ERROR');
     });
